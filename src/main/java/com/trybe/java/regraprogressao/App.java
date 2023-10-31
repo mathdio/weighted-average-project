c5d4a690c65437b02fa28c347e54bdf1a9b5dc85
Matheus Diógenes <mathdiogenes@gmail.com>
2023-10-31 13:38:11 -0300

    feat: R1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,26 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    register();
+  }
+
+  /**
+   * Method to register activities.
+   */
+  public static void register() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int activityQnt = Integer.parseInt(scanner.nextLine());
+
+    for (int i = 1; i <= activityQnt; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String name = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade " + i + ":");
+      int activityWeight = Integer.parseInt(scanner.nextLine());
+    }
+
+    scanner.close();
   }
 }
