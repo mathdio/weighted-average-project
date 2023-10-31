4e795c1488582899ec5b7fea2a784ad3c154e981
Matheus Diógenes <mathdiogenes@gmail.com>
2023-10-31 14:00:54 -0300

    feat: R4

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -19,7 +19,8 @@ public class App {
    */
   public static void register() {
     Scanner scanner = new Scanner(System.in);
-    int weightSum = 0;
+    int weightsSum = 0;
+    int gradesSum = 0;
 
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     int activityQnt = Integer.parseInt(scanner.nextLine());
@@ -30,14 +31,26 @@ public class App {
 
       System.out.println("Digite o peso da atividade " + i + ":");
       int activityWeight = Integer.parseInt(scanner.nextLine());
-      weightSum += activityWeight;
+      weightsSum += activityWeight;
 
       System.out.println("Digite a nota obtida para " + activityName + ":");
       int activityGrade = Integer.parseInt(scanner.nextLine());
+      gradesSum += (activityWeight * activityGrade);
     }
 
-    if (weightSum != 100) {
+    if (weightsSum != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
+    }
+
+    double finalGrade = (double) gradesSum / weightsSum;
+
+    if (finalGrade >= 85.0) {
+      System.out.println("Parabéns! Você alcançou " + finalGrade + "%! "
+          + "E temos o prazer de informar que você obteve aprovação! ");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação alcançada neste "
+          + "período, " + finalGrade + "%, você não atingiu a "
+          + "pontuação mínima necessária para sua aprovação.");
     }
 
     scanner.close();
